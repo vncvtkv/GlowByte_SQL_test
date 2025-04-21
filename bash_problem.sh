81692197b7d7f143a3161886073562ccc8467a0b
Ivan Tsvetkov <124565814+vncvtkv@users.noreply.github.com>
2025-04-21 10:25:57 +0300

    Update bash_problem.sh

--- bash_problem.sh
+++ bash_problem.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat log.txt | grep -oP 'UCP_PARALLELRUN:[^,]+' | sort -u | sed 's/.*/disable &/' | tr ',' '\n' > output.txt
+cat log.txt | grep -oP 'UCP_PARALLELRUN:[^,]+' | sort -u | sed 's/.*/disable & \n/'  > output.txt
